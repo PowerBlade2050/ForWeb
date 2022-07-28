5394eb0cac9faef988a0f6972ebad76874d9d79f
PowerBlade2050 <power.blade.2050@gmail.com>
2022-07-28 16:53:47 +0200

    exactly same from react-router-dom

--- portfolio/src/Page/index.js
+++ portfolio/src/Page/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router';
+import { Routes, Route } from 'react-router-dom';
 
 // Import contenu page
 import Accueil from './Accueil';
