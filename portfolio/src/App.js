21b343abc63b05100e46aba8fc3810ccbf6ae4a5
PowerBlade2050 <power.blade.2050@gmail.com>
2022-07-28 16:24:49 +0200

    architecture from src ok

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,26 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.scss';
 import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <Header />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React gregr
-        </a>
-      </header>
+      <Header />
+      <Footer />
     </div>
   );
 }
--- portfolio/src/Header/index.js
+++ portfolio/src/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => (
     <header className="header">
-        <h1 className="header_title">Bienvenue !!</h1>
+        <h1 className="header_title">Power Blade 2050</h1>
     </header>
 );
 export default Header;
